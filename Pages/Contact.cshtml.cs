b6b6b263de06253985df427e2737e47ab7230279
Beinjamin <jauresbeinjamin@gmail.com>
2024-05-14 00:58:30 +0100

    Suppresion fichier lourd et refronte

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetCameroon.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+            this.SetupViewDataTitleFromUrl();
+        }
+    }
+}
