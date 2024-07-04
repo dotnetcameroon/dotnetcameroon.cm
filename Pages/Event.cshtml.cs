eef7e4c662c5c9228fc326a7888c65762ad05937
Beinjamin <jauresbeinjamin@gmail.com>
2024-07-04 21:28:45 +0100

    Creation page event

---
+++ Pages/Event.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNet_Cameroon.Pages
+{
+    public class EventModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
